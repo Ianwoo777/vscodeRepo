002177e36c13f5dbfb5792bcb5fe17b298ee67dd
IanWoo <dtwy77@gmail.com>
2018-07-19 09:30:23 +0800

    aa

--- Py_nut1.py
+++ Py_nut1.py
@@ -94,7 +94,7 @@ def getTitle(url):
         return None
     return title
 
-###aaaabbccdd
+###aaaabbccdd6
 html=urlopen('http://www.pythonscraping.com/pages/warandpeace.html')
 bsobj=BeautifulSoup(html)
 namelist=bsobj.findAll("span", {"class":"green"})
---
+++ descriptor.py
@@ -0,0 +1,70 @@
+#base class, uses a descriptor to set a value
+class Descriptor:
+    def __init__(self, name=None, **opts):
+        self.name=name
+        for key, value in opts.items():
+            setattr(self, key, value)
+
+    def __set__(self, instance, value):
+        instance.__dict__[self.name]=value
+
+#Descriptor for enforcing types
+class Typed(Descriptor):
+    expected_type=type(None)
+
+    def __set__(self, instance, value):
+        if not isinstance(value, self.expected_type):
+            raise TypeError('expected '+str(self.expected_type))
+        super().__set__(instance, value)
+
+#descriptor for enforctin values
+class Unsigned(Descriptor):
+    def __set__(self, instance, value):
+        if value<0:
+            raise ValueError('expected >=0')
+        super().__set__(instance, value)
+
+class MaxSized(Descriptor):
+    def __init__(self, name=None, **opts):
+        if 'size' not in opts:
+            raise TypeError('missing size option')
+        super().__init__(name, **opts)
+
+    def __set__(self, instance, value):
+        if len(value)>=self.size:
+            raise ValueError('size must be < '+str(self.size))
+        super().__set__(instance, value)
+
+class Integer(Typed):
+    expected_type=int
+
+class UnsignedInteger(Integer, Unsigned):
+    pass
+
+class Float(Typed):
+    expected_type=float
+
+class UnsignedFloat(Float, Unsigned):
+    pass
+
+class String(Typed):
+    expected_type=str
+
+class SizedString(String, MaxSized):
+    pass
+
+#using these objects, is now possible to define a class such as:
+class Stock:
+    #specify constraints
+    name=SizedString('name', size=8)
+    shares=UnsignedInteger('shares')
+    price=UnsignedFloat('price')
+    def __init__(self, name, shares, price):
+        self.name=name
+        self.shares=shares
+        self.price=price
+
+#
+s=Stock('acem', 50, 80.8)
+print(s.name)
+s.price='alot'
